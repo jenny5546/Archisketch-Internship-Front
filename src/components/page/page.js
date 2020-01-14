215005e22f48bfd31c71a18d463ca51f0d37e947
jenny5546 <jenny5546@naver.com>
2020-01-14 10:49:47 +0900

    added css to main

--- src/components/page/page.js
+++ src/components/page/page.js
@@ -16,29 +16,36 @@ class Page extends Component {
     render() {
         return(
             <div className="landing">
+                <div className="landing__innerwrap">
+                    <div className="landing__image">
+                        {/* <img></img> */}
+                    </div>
+                    <div className="landing__message">
+                        <hr className="landing__message-line"/>
+                        <div className= "landing__message-heading">
+                            ARCHISKETCH/ON-DEMAND
+                        </div>
+                        <div className= "landing__message-subheading">
+                            <p className= "landing__message-paragraph">We turn your boring  </p>
+                            <p className= "landing__message-paragraph">floorplan images to 3D Virtual Tours</p> 
+                        </div>
+                        <div className= "landing__message-text">
+                            We create accurate 2D, 3D models of your floorplans and furnish them according to specifications.
+                            With our expertise in interior design and technological edge, we promise you accurate and powerful digital spaces.
+                        </div>
+                        
+                        <button className="landing__button" onClick={e => {this.showModal();}}>
+                        Click to Get Started
+                        </button>
+
+                    </div>
+        
+                    
+                    <div className="landing__modal">
+                        <Modal show={this.state.show} onClose={this.showModal}/>
+                    </div>
+                </div>
                 
-                <div className="landing__image">
-                    {/* <img></img> */}
-                </div>
-                <div className="landing__message">
-                    <div className= "landing__message-heading">
-                        ARCHISKETCH/ON-DEMAND
-                    </div>
-                    <div className= "landing__message-subheading">
-                        We turn your boring floorplan images to 3D Virtual Tours 
-                    </div>
-                    <div className= "landing__message-text">
-                        We create accurate 2D, 3D models of your floorplans and furnish them according to specifications.
-                        With our expertise in interior design and technological edge, we promise you accurate and powerful digital spaces
-                    </div>
-                </div>
-    
-                <button className="landing__button" onClick={e => {this.showModal();}}>
-                    Click to Get Started
-                </button>
-                <div className="landing__modal">
-                    <Modal show={this.state.show} onClose={this.showModal}/>
-                </div>
             </div> 
         );
     }
